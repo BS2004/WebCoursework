e3d9ae1db0cf97ad57eb6ebb526a86684c102bfb
Ben Stewart <118538101+BS2004@users.noreply.github.com>
2025-04-16 22:50:13 +0100

    Update courses.js

--- db/courses.js
+++ db/courses.js
@@ -1,4 +1,4 @@
-const Datastore = require('nedb');
+const Datastore = require('gray-nedb');
 const coursesDB = new Datastore({ filename: 'db/courses.db', autoload: true });
 
 module.exports = coursesDB;
